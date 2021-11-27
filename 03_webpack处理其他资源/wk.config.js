e479f983f91f4e938ec92cc291d1346f89083297
panyue <90938238g@gmail.com>
2021-11-27 09:14:24 +0800

    file-loader,url-loader处理文件打包

---
+++ 03_webpack处理其他资源/wk.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+//common js方式导出(webpack的配置文件)
+module.exports = {
+    entry: './src/main.js', //入口文件
+    output: {
+        filename: "bundle.js", //打包后的文件名
+        path: path.resolve(__dirname, './build'), //__dirname:当前文件所在目录的绝对路径 注意这里一定要使用绝对路径
+    },
+    module:{
+        rules: [
+            {
+                test:/\.css$/i, // '正则表达式匹配资源'
+                // loader: 'css-loader'
+                use:[
+                    // { loader: 'css-loader' }
+                    //注意webpack默认是从下往上(写在一排就是从右往左处理loader的)处理loader (从后往前)
+                    //注意编写顺序
+                    'style-loader', //简写只有一个loader的时候(没有其他的选项需要处理时)是可以直接传递字符串的
+                    // 'css-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1, //设置css-loader的importLoaders属性为1，表示css-loader处理完后某个css文件之后如果这个css文件里还有@import,则还会采用postcss-loader继续处理该css
+                        }
+                    },
+                    "postcss-loader"
+                    // {
+                    //     loader: 'postcss-loader',
+                    //     options: {
+                    //         postcssOptions:  {
+                    //             //依赖的插件
+                    //             plugins: [
+                    //                 //浏览器前缀
+                    //                 // require('autoprefixer'),
+                    //                 //postcss-preset-env已经包含了autoprefixer的功能了
+                    //                 // require('postcss-preset-env')
+                    //                 "postcss-preset-env", //简写
+                    //             ]
+                    //         }
+                    //     }
+                    // }
+                ]  //use-entry
+            },
+            {
+                test: /\.less$/,
+                use:[
+                    //注意顺序 处理less文件的loader less->css->document中的style标签
+                    'style-loader',
+                    {
+                        loader:'css-loader',
+                        options:{
+                            importLoaders:2, //css-loader处理之前，还需要处理less文件中的@import
+                        }
+                    },
+                    "postcss-loader",
+                    'less-loader',
+                ]
+            },
+            {
+                test:/\.(png|jpe?g|gif|svg)$/i, //处理图片的规则
+                use: [
+                    {
+                        // loader:'file-loader',
+                        //把图片直接转换为base64格式的字符串
+                        //原则:小图片base64格式，大图片直接http请求
+                        loader: 'url-loader',
+                        options: {
+                            //重命名
+                            name: '[name].[hash:6].[ext]',
+                            outputPath: 'images/', //输出路径
+                            limit: 100 * 1024, //小于100k的图片转换为base64格式的字符串
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
