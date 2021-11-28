7813953318cf2b51ce05140a8a2f31905c01b56f
panyue <90938238g@gmail.com>
2021-11-28 09:11:50 +0800

    测试主文件index.js

---
+++ 06_webpack模块化原理/src/index.js
@@ -0,0 +1,9 @@
+//采用commonjs的方式导入esmodule导出的模块
+const { add } = require('./js/add.js');
+//commonjs导出内容 esmodule导入内容
+import { hello } from './js/hello';
+
+console.log(add(1,2));
+console.log(hello('py'));
+
+// console.log(abc)
