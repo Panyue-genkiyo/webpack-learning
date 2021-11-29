73e544394b786ca116427a10bfb5ab0c5413bdb6
panyue <90938238g@gmail.com>
2021-11-29 11:04:51 +0800

    测试加载的vue文件

---
+++ 10_webpack打包vue代码/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+   <div id="app">
+      <h1 class="title">{{ msg }}</h1>
+   </div>
+</template>
+
+
+<script>
+  export default {
+     data(){
+       return {
+         msg: 'welcome to js learning'
+       }
+     }
+  }
+</script>
+
+<style  scoped lang="less">
+    .title{
+      color: red;
+    }
+</style>
