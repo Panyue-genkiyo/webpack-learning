af1c4bdcef9ac4239b8cd64bbbc60781333e68fa
panyue <90938238g@gmail.com>
2021-12-02 21:25:50 +0800

    hash_chunkhash_contenthash三者的区别

---
+++ 16_hash_chunkhash_contenthash/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const isProduction = true;
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+   mode: 'production', //生产环境
+   optimization:{
+     chunkIds:'deterministic',
+   },
+  //不需要打包三方库 把这些资源全部让用户从cdn上下载下来
+  externals:{
+    loadsh: "_",
+    dayjs: 'dayjs'
+  },
+   plugins:[
+     new CleanWebpackPlugin({}),
+     //生产环境下拆包css文件
+     new MiniCssExtractPlugin({
+       //打包对应的hash值是与本次内容相关的
+       filename: 'css/[name].[contenthash:8].css',
+       // chunkFilename: 'css/[name].[chunkhash:8].css',
+     })
+   ]
+}
---
+++ 16_hash_chunkhash_contenthash/src/main.js
@@ -0,0 +1,6 @@
+// import _ from 'loadsh';
+// import dayjs from 'dayjs';
+import './style.css';
+console.log('hello main.jsssddd dddd ss');
+// console.log(dayjs(), 'main.js')
+console.log('test');
