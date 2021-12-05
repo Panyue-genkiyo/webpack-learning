f325d8e32eef3cbc9fafe373a6b77f85824e12f6
panyue <90938238g@gmail.com>
2021-12-05 16:39:38 +0800

    测试文件

---
+++ 23_gulp的基本使用/src/js/math.js
@@ -0,0 +1,5 @@
+export function sum(n1, n2){
+  return n1 + n2;
+}
+
+console.log('math.js')
---
+++ 23_gulp的基本使用/src/script/main.js
@@ -0,0 +1,8 @@
+const msg = 'hello,gulp';
+console.log(msg);
+
+const foo = () => {
+   console.log('function foo');
+}
+
+foo();
